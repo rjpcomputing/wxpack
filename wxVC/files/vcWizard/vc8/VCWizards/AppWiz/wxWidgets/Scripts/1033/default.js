2f75ffeafea4dd3be66eb8b088936c230f412860
rjmyst3 <rjmyst3@9ef50a6c-d019-0410-a6fa-efe1ad73bb17>
2006-12-22 21:17:05 +0000

    updated to version 28

--- wxVC/files/vcWizard/vc8/VCWizards/AppWiz/wxWidgets/Scripts/1033/default.js
+++ wxVC/files/vcWizard/vc8/VCWizards/AppWiz/wxWidgets/Scripts/1033/default.js
@@ -237,11 +237,11 @@ function AddConfig(proj, strProjectName)
 				{
 				    if ( wizard.FindSymbol( 'WX_USE_UNICODE' ) )
 				    {
-						additionalDepends += "wxmsw27ud.lib ";
+						additionalDepends += "wxmsw28ud.lib ";
 					}
 					else
 					{
-					    additionalDepends += "wxmsw27d.lib ";
+					    additionalDepends += "wxmsw28d.lib ";
 					}
 				}
 				LinkTool.LinkIncremental = linkIncrementalYes;
@@ -253,11 +253,11 @@ function AddConfig(proj, strProjectName)
 				{
 					if ( wizard.FindSymbol( 'WX_USE_UNICODE' ) )
 				    {
-						additionalDepends += "wxmsw27u.lib ";
+						additionalDepends += "wxmsw28u.lib ";
 					}
 					else
 					{
-					    additionalDepends += "wxmsw27.lib ";
+					    additionalDepends += "wxmsw28.lib ";
 					}
 				}
 				LinkTool.LinkIncremental = linkIncrementalNo;
