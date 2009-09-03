b130c417d48ecb66d7fe5960c1615870b1a886ba
rjpcomputing <rjpcomputing@9ef50a6c-d019-0410-a6fa-efe1ad73bb17>
2009-09-03 21:20:23 +0000

    Adding comments during the build. Now coping 'setup.h' to the include/wx directory.

--- build_wxCompiled.bat
+++ build_wxCompiled.bat
@@ -1,29 +1,36 @@
 @echo off
-:: Copy the setup configuration into the source tree
+echo Copy the setup configuration into the source tree
 copy setup.h /Y wxwidgets\include\wx\msw
+copy setup.h /Y wxwidgets\include\wx
 
-:: Copy build script to build directory
+echo Copy build script to build directory
 copy wxBuild_wxWidgets.bat /Y wxwidgets\build\msw
 
-:: Change into the build directory of the source tree
+::echo Create directories for binary targets
+::mkdir wxwidgets\lib\gcc_dll
+::mkdir wxwidgets\lib\gcc_lib
+::mkdir wxwidgets\lib\vc_dll
+::mkdir wxwidgets\lib\vc_lib
+
+echo Change into the build directory of the source tree
 cd wxwidgets\build\msw
 
-:: Run the build file for each compiler
+echo Run the build file for each compiler
 call wxBuild_wxWidgets.bat VC80 ALL
 call wxBuild_wxWidgets.bat MINGW4 ALL
 
-:: Move the built binaries to save them from being overwritten
+echo Move the built binaries to save them from being overwritten
 call wxBuild_wxWidgets.bat VC80 MOVE
 call wxBuild_wxWidgets.bat MINGW4 MOVE
 
-:: Change to installer directory
+echo Change to installer directory
 cd ..\..\..\install\wxCompiled 
 
 :: Make installer for wxCompiled
 echo Building installer...
 :: "C:\Program Files\Inno Setup 5\Compil32.exe" /cc "wxWidgets Compiled.iss"
 
-:: Change to additions build directory
+echo Change to additions build directory
 cd ..\..\wxwidgets\additions\build
 call build_wxadditions VC80
 call build_wxadditions MINGW4
