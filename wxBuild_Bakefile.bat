7d727e9b7064429c0a4ab0aec16eab2d6591f1c9
csteenwyk <csteenwyk@gmail.com>
2015-11-21 20:53:45 -0500

    Cleaned up bakefile

--- wxBuild_Bakefile.bat
+++ wxBuild_Bakefile.bat
@@ -1,25 +1,21 @@
 if (%WXBUILD_3RD_PARTY%) == () goto END
+if not exist "%WXBUILD_3RD_PARTY%\include\jpeg-turbo\%COMPILER_NAME%" goto END
 
-if exist "%WXBUILD_3RD_PARTY%\include\jpeg-turbo\%COMPILER_NAME%" (
-	cd ..\bakefiles
-	set BAKE_LIB_SUFFIX=%COMPILER_ARCH%
-	if (%COMPILER_ARCH%) == (32) set BAKE_LIB_SUFFIX=
+cd ..\bakefiles
+set BAKE_LIB_SUFFIX=%COMPILER_ARCH%
+if (%COMPILER_ARCH%) == (32) set BAKE_LIB_SUFFIX=
 
-	echo baking new makefiles for external 3rd party libs...
-	
-	bakefile -f %BAKE_FORMAT% ^
-	-DwxUSE_LIBJPEG=sys -DLIB_JPEG="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\jpeg-static" -DINC_JPEG="%WXBUILD_3RD_PARTY%\include\jpeg-turbo\%COMPILER_NAME%\%1\runtime-static\x%COMPILER_ARCH%" ^
-	-DwxUSE_LIBPNG=sys -DLIB_PNG="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\png" -DINC_PNG="%WXBUILD_3RD_PARTY%\include\png" ^
-	-DwxUSE_LIBTIFF=sys -DLIB_TIFF="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\tiff" -DINC_TIFF="%WXBUILD_3RD_PARTY%\include\tiff" ^
-	-DwxUSE_ZLIB=sys -DLIB_ZLIB="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\z" -DINC_ZLIB="%WXBUILD_3RD_PARTY%\include\zlib" ^
-	-o../msw/%MAKEFILE% -DOPTIONS_FILE=%BAKE_OPTIONS_FILE% wx_no_3rd_party.bkl
+echo baking new makefiles for external 3rd party libs...
 
+bakefile -f %BAKE_FORMAT% ^
+-DwxUSE_LIBJPEG=sys -DLIB_JPEG="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\jpeg-static" -DINC_JPEG="%WXBUILD_3RD_PARTY%\include\jpeg-turbo\%COMPILER_NAME%\%1\runtime-static\x%COMPILER_ARCH%" ^
+-DwxUSE_LIBPNG=sys -DLIB_PNG="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\png" -DINC_PNG="%WXBUILD_3RD_PARTY%\include\png" ^
+-DwxUSE_LIBTIFF=sys -DLIB_TIFF="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\tiff" -DINC_TIFF="%WXBUILD_3RD_PARTY%\include\tiff" ^
+-DwxUSE_ZLIB=sys -DLIB_ZLIB="%WXBUILD_3RD_PARTY%\lib%BAKE_LIB_SUFFIX%\%COMPILER_NAME%\link-static\runtime-dynamic\%1\z" -DINC_ZLIB="%WXBUILD_3RD_PARTY%\include\zlib" ^
+-o../msw/%MAKEFILE% -DOPTIONS_FILE=%BAKE_OPTIONS_FILE% wx_no_3rd_party.bkl
 
-	echo baked
+echo baked
 
-	cd ..\msw
-) else (
-	echo bake skipped because third party directories do not exist
-)
+cd ..\msw
 
 :END
